a8036a7785f7b9f78bd80386ab633f7774d9482b
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2026-01-22 01:14:34 +0530

    Enhance TestString with additional string operations
    Added string length, character access, and substring methods.

--- Datatypes/TestString.java
+++ Datatypes/TestString.java
@@ -4,5 +4,12 @@ public class TestString {
     public static void main(String[] args){
         String name = "Adheesh";
         System.out.println(name);
+        int length = name.length();
+        System.out.println(length);
+        System.out.println(name.charAt(length-1));
+        String name2 = "AdheeshChopra";
+        System.out.println(name.equals(name2)); //false
+        System.out.println(name.substring(4));
     }
 }
+
