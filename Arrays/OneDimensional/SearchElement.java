5a2370b1d051f08f8cbb5899f612557310abb701
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2026-01-24 01:35:37 +0530

    Create SearchElement.java

---
+++ Arrays/OneDimensional/SearchElement.java
@@ -0,0 +1,13 @@
+package Arrays.OneDimensional;
+
+public class SearchElement {
+    public static void main(String[] args){
+//        Search an Element in an array
+
+        int[] arr ={1,4,2,55,7,-2};
+
+        for(int i: arr) if(i==55) System.out.println("Found "+ i);
+
+
+    }
+}
