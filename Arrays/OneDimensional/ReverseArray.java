2870cf5ce4e1781d8c2805c545e1d4a7c0523508
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2026-01-24 01:34:28 +0530

    Create ReverseArray.java

---
+++ Arrays/OneDimensional/ReverseArray.java
@@ -0,0 +1,11 @@
+package Arrays.OneDimensional;
+
+public class ReverseArray {
+    public static void main(String[] args){
+        int[] arr={1,3,43,4,345,53,5,4};
+
+        for(int i= arr.length -1; i>=0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
